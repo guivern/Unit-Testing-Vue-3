3e069ff24d9f3f77a5b54f086db61645e2a8641b
Guillermo Verón <guiver95@gmail.com>
2022-07-04 21:58:57 -0400

    a simple unit testing

--- unit-testing-vue/tests/unit/AppHeader.spec.js
+++ unit-testing-vue/tests/unit/AppHeader.spec.js
@@ -1,15 +1,16 @@
 import AppHeader from '@/views/AppHeader'
 import { mount } from '@vue/test-utils'
 
-describe('AppHeader', () => {
+describe('AppHeader', () => { // create a block of test
+  // set up your tests
   test('If user is not logged in, do not show logout button', () => {
-    const wrapper = mount(AppHeader)
-    expect(wrapper.find('button').isVisible()).toBe(false)
+    const wrapper = mount(AppHeader) // mount the component
+    expect(wrapper.find('button').isVisible()).toBe(false) // assert what the result should be
   })
 
-  test('If user is logged in, show logout button', async () => {
-    const wrapper = mount(AppHeader)
-    await wrapper.setData({ loggedIn: true })
-    expect(wrapper.find('button').isVisible()).toBe(true)
+  test('If user is logged in, show logout button', async () => { // using async/await when waiting on DOM updates
+    const wrapper = mount(AppHeader) // mount the component
+    await wrapper.setData({ loggedIn: true }) // set data
+    expect(wrapper.find('button').isVisible()).toBe(true) // assert what the result should be
   })
 })
