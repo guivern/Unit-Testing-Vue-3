23e5fd981f57bf507b52a457fa9a1cd78f2db3c4
Guillermo Verón <guiver95@gmail.com>
2022-07-05 22:26:56 -0400

    testing props

---
+++ tests/unit/RandomNumber.spec.js
@@ -0,0 +1,30 @@
+import RandomNumber from '@/views/RandomNumber'
+import { mount } from '@vue/test-utils'
+
+describe('RandomNumber', () => {
+  const wrapper = mount(RandomNumber)
+  test('By default randomNumber data value should be 0', () => {
+    expect(wrapper.html()).toContain('<span>0</span>')
+  })
+
+  test('randomNumber data value should be between 1 and 10', async () => {
+    const wrapper = mount(RandomNumber)
+    await wrapper.find('button').trigger('click')
+    const randomNumber = parseInt(wrapper.find('span').text())
+    expect(randomNumber).toBeGreaterThanOrEqual(1)
+    expect(randomNumber).toBeLessThan(10)
+  })
+
+  test('randomNumber data value should be between 100 and 200', async () => {
+    const wrapper = mount(RandomNumber, {
+      props: {
+        min: 100,
+        max: 200,
+      },
+    })
+    await wrapper.find('button').trigger('click')
+    const randomNumber = parseInt(wrapper.find('span').text())
+    expect(randomNumber).toBeGreaterThan(100)
+    expect(randomNumber).toBeLessThan(200)
+  })
+})
